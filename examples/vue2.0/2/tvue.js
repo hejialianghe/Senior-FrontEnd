7a4cf8a41b3aef38d8d861a288a312fb5a515ae1
hejialiang <hejialiang@gxdataway.com>
2020-02-26 17:13:13 +0800

    vue源码实现响应示

---
+++ examples/vue2.0/2/tvue.js
@@ -0,0 +1,23 @@
+class tvue {
+    constructor (options){
+        this.$options=options
+        
+        // 数据响应化
+        this.$data=options.data
+        this.observe(this.$data)
+    }
+    observe(value){
+      if(Array.isArray(value)){
+
+      }else{
+          this.walk(value)
+      }
+    }
+    walk(obj){
+        const keys=Object.keys(obj)
+        for(let i=0;i<keys.length;i++){
+            // 传入对象和属性名
+            defineReactive(obj,key[i])
+        }
+    }
+}
