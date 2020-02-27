b36b0d8012e53a1fb03509a6f683782d567fbfa6
hejialiang <984714129@qq.com>
2020-02-27 23:08:53 +0800

    添加watcher和dep
    Signed-off-by: hejialiang <984714129@qq.com>

--- examples/vue2.0/2/tvue.js
+++ examples/vue2.0/2/tvue.js
@@ -1,12 +1,12 @@
 class tvue {
     constructor (options){
-     console.log('options',options)
         this.$options=options
     
         // 数据响应化
         this.$data=options.data
         this.observe(this.$data)
-        this.$data.age=23
+   
+        new Compile(options.el,this)
     }
     observe(value){
       if(Array.isArray(value)){
@@ -26,27 +26,59 @@ class tvue {
     }
     defineReactive(obj,key,val){
         val=obj[key]
+        const dep=new Dep()
         Object.defineProperty(obj,key,{
             get:function reactiveGetter(){
-        
+                if(Dep.target){
+                    dep.addDep(Dep.target)
+                }
                 return val
             },
             set:function reactiveSetter (newVal){
-                console.log(8888888888888, newVal)
+               dep.notify()
                val=newVal
             }
         })
     }
     proxyData (key) {
+        Object.defineProperty(this,key,{
+            get(){
+              return this.$data[key]  
+            },
+            set(newVal){
+                console.log(222, newVal)
+                this.$data[key]=newVal
+            }
+        })
 
     }
 }
-const vm= new tvue({
-    el: "#app",
-    data: {
-      name: "I am test.",
-      age: 12,
-      html: "<button>这是一个按钮</button>"
+// Dep用来管理watcher
+class Dep {
+    constructor(){
+        // 存放watcher实例
+        this.deps=[]
     }
-})
-console.log(vm)
+    addDep(dep){
+        this.deps.push(dep)
+    }
+    notify(){
+        this.deps.forEach(dep=>dep.update())
+    }
+}
+
+class Watcher {
+    constructor(vm,key,cb){
+        this.vm=vm;
+        this.key=key;
+        this.cb=cb
+        // 把当前的wathcer实例指定到Dep静态属性target上
+        Dep.target=this;
+        this.vm[this.key] // 触发getter添加依赖
+        Dep.target=null
+    }
+    update(){
+        console.log('通知了')
+        this.cb.call(this.vm,this.vm[this.key])
+    }
+}
