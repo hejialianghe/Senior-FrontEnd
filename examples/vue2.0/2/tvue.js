12deb09c5694bc11ec8f3eb73032464b2a7f657f
hejialiang <hejialiang@gxdataway.com>
2020-02-27 17:08:18 +0800

    vue响应式

--- examples/vue2.0/2/tvue.js
+++ examples/vue2.0/2/tvue.js
@@ -1,10 +1,12 @@
 class tvue {
     constructor (options){
+     console.log('options',options)
         this.$options=options
-        
+    
         // 数据响应化
         this.$data=options.data
         this.observe(this.$data)
+        this.$data.age=23
     }
     observe(value){
       if(Array.isArray(value)){
@@ -15,10 +17,36 @@ class tvue {
     }
     walk(obj){
         const keys=Object.keys(obj)
-        for(let i=0;i<keys.length;i++){
+        keys.forEach(key=>{
             // 传入对象和属性名
-            defineReactive(obj,key[i])
-        }
+            this.defineReactive(obj,key,obj[keys])
+            // 代理data中的属性到vue 实例上
+            this.proxyData(key)
+        })
+    }
+    defineReactive(obj,key,val){
+        val=obj[key]
+        Object.defineProperty(obj,key,{
+            get:function reactiveGetter(){
+        
+                return val
+            },
+            set:function reactiveSetter (newVal){
+                console.log(8888888888888, newVal)
+               val=newVal
+            }
+        })
+    }
+    proxyData (key) {
+
     }
 }
-console.log([(new Array(6)).fill(0)])
+const vm= new tvue({
+    el: "#app",
+    data: {
+      name: "I am test.",
+      age: 12,
+      html: "<button>这是一个按钮</button>"
+    }
+})
+console.log(vm)
