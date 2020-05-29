231a71e391bbc8ace44d8c523bd0c372e5a76f96
hejialiang <hejialiang@gxdataway.com>
2020-05-29 13:22:58 +0800

    加入部署脚本

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!usr/bin/env sh
+
+#当发生错误中止脚本
+
+set -e
+
+#构建
+npm run build
+
+# cd 到构建输出的目录下
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/hejialianghe/hejialianghe.github.io master
+
+ cd -
