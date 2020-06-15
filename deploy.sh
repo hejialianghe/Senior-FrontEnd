f8782097ea3c3140d92979a2dec8589d0dc2ed0b
hejialiang <hejialiang@gxdataway.com>
2020-06-15 16:32:13 +0800

    修改路径错误

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://${access_token}github.com/hejialianghe/hejialianghe.github.io master
+git push -f https://${access_token}@github.com/hejialianghe/hejialianghe.github.io master
 
  cd -
