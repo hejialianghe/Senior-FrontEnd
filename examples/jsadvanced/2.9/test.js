f97d408a8b6198a460740d644ecf2309eee135a5
hejialiang <hejialiang@gxdataway.com>
2020-10-14 09:12:03 +0800

    修改错误

--- examples/jsadvanced/2.9/test.js
+++ examples/jsadvanced/2.9/test.js
@@ -1,10 +1,13 @@
-const arr=[3,1,5,9,4]
+let arr=[3,5,2,9,10,4,88]
 
 for(let i=0;i<arr.length;i++){
-    let _arr=[...arr]
-    for(let j=0;j<arr.length;j++){
-        if(arr[i]>arr[j]){
-            [arr[i],arr[i+1]]=[_arr[j+1],_arr[j]]
-        }
+    let current =arr[i+1]
+    let preIndex=i
+    while(preIndex>=0 && (current<arr[preIndex])){
+        arr[preIndex+1]= arr[preIndex]
+        arr[preIndex]=current
+        preIndex--
     }
 }
+
+console.log(arr);
