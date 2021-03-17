43cb803fbf5f8f160cfd9830d8cba5b45580cc29
hejialiang <984714129@qq.com>
2021-03-17 23:28:29 +0800

    feat：cookie

--- examples/computerNetwork/4.2/cookie.js
+++ examples/computerNetwork/4.2/cookie.js
@@ -1,8 +1,25 @@
 const express =  require('express')
-
 const app1 = express()
+const app2 = express()
+
+app1.set('etag',false)
 app1.get('/',(req,res)=>{
-    res.setHeader('Set-Cookie','abc=123')
+    res.setHeader('Set-Cookie','abc=123;SameSite=None;Secure')
     res.send('ok')
 })
+
+
+app2.get('/',(req,res)=>{
+ res.setHeader('Set-Cookie','apc=123')
+ res.setHeader(
+     "Access-Control-Allow-Origin",
+     "http://www.dev.com"
+ )
+ res.setHeader(
+    "Access-Control-Allow-Credentials",
+    "true"
+)
+ res.send('ok')
+})
 app1.listen(3000)
+app2.listen(3001)
