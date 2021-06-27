dca60cb5082a8b3d6f9a12d3e72a4e5de74b5d88
hejialiang <984714129@qq.com>
2021-06-27 16:47:51 +0800

    feat:添加changelog

--- html/test.js
+++ html/test.js
@@ -1,16 +1,7 @@
-const sub = {
-  'firstname_1_1':"jl",
-  'lastname_2_1':"h"
+
+const a={
+  b:1
 }
-const result = []
-for(item of Object.keys(sub)) {
-  const keys = item.split('_')
-  if(result[keys[1]]){
-    result[keys[1]]=[{
-      firstname:sub[fis]
-      lastname:
-    }]
-  }
-    const arr = []
-  console.log(result[keys[0]])
-}
+const b=a
+b.b=4
+console.log(a)
