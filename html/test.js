e627cc56659a4abc003235e64ad03dc292c6ba46
hejialiang <984714129@qq.com>
2021-08-19 23:43:08 +0800

    fix：修改排序

--- html/test.js
+++ html/test.js
@@ -1,17 +1,15 @@
-let usecallback
-let count = {a:1}
-function fn (params) {
-    const data=count
-    console.log('----',data)
-    if(usecallback){
-        usecallback()
-    }else {
-        usecallback=()=>{
-            console.log(data.a)
-        }
-        usecallback()
+const  arr = [5,6,2,8,4,3,1]
+let max
+for(let i=0;i<arr.length-1;i++){
+    console.log(arr)
+    for(let j=0;j<arr.length-1-i;j++){
+
+    if(arr[j]>arr[j+1]){
+        max=arr[j]
+        arr[j]= arr[j+1]
+        arr[j+1]=max
+    }
     }
 }
-fn()
-count.a=2
-fn()
+
+console.log('arr',arr)
