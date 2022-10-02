65053a1519bfd87475a63f4f651cb77bf95d7a4c
hejialiang <984714129@qq.com>
2022-10-02 18:08:07 +0800

    feat: gitee 重定向
    n

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -5,48 +5,20 @@
     @touchstart="onTouchStart"
     @touchend="onTouchEnd"
   >
+    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
+    <div class="sidebar-mask" @click="toggleSidebar(false)"></div>
 
-
-    <Navbar
-      v-if="shouldShowNavbar"
-      @toggle-sidebar="toggleSidebar"
-    />
-    <div
-      class="sidebar-mask"
-      @click="toggleSidebar(false)"
-    ></div>
-
-    <Sidebar
-      :items="sidebarItems"
-      @toggle-sidebar="toggleSidebar"
-    >
-      <slot
-        name="sidebar-top"
-        #top
-      />
-      <slot
-        name="sidebar-bottom"
-        #bottom
-      />
+    <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
+      <slot name="sidebar-top" #top />
+      <slot name="sidebar-bottom" #bottom />
     </Sidebar>
 
-    <Home v-if="$page.frontmatter.home"/>
+    <Home v-if="$page.frontmatter.home" />
 
-    <Page
-      v-else
-      :sidebar-items="sidebarItems"
-    >
-      <slot
-        name="page-top"
-        #top
-      />
-      <slot
-        name="page-bottom"
-        #bottom
-      />
-
+    <Page v-else :sidebar-items="sidebarItems">
+      <slot name="page-top" #top />
+      <slot name="page-bottom" #bottom />
     </Page>
-          
   </div>
 </template>
 
@@ -56,60 +28,61 @@ import Navbar from '@theme/components/Navbar.vue'
 import Page from '@theme/components/Page.vue'
 import Sidebar from '@theme/components/Sidebar.vue'
 import { resolveSidebarItems } from '../util'
-import * as Sentry from "@sentry/browser";
-import { Integrations } from "@sentry/tracing";
+import * as Sentry from '@sentry/browser'
+import { Integrations } from '@sentry/tracing'
 //  sentry异常监控
 Sentry.init({
-  dsn: "https://39616798988c45a88e52d89282f7dcd1@o522017.ingest.sentry.io/5632836",
+  dsn: 'https://39616798988c45a88e52d89282f7dcd1@o522017.ingest.sentry.io/5632836',
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
-});
+})
 
 export default {
   components: { Home, Page, Sidebar, Navbar },
 
-  data () {
+  data() {
     return {
-      isSidebarOpen: false
+      isSidebarOpen: false,
     }
   },
-  beforeMount(){
-  // 百度埋点
-    var _hmt = _hmt || [];
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?a56e4bf87056c895a1e9e9d38298da40";
-    var s = document.getElementsByTagName("body")[0];
-    s.appendChild(hm);
+  beforeMount() {
+    // 百度埋点
+    var _hmt = _hmt || []
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?a56e4bf87056c895a1e9e9d38298da40'
+    var s = document.getElementsByTagName('body')[0]
+    s.appendChild(hm)
 
+    if (location.origin.indexOf('gitee') >= 0) {
+      location.href = 'https://senior-frontend.pages.dev'
+    }
   },
   computed: {
-    shouldShowNavbar () {
+    shouldShowNavbar() {
       const { themeConfig } = this.$site
       const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
+      if (frontmatter.navbar === false || themeConfig.navbar === false) {
         return false
       }
       return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
+        this.$title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav ||
+        this.$themeLocaleConfig.nav
       )
     },
 
-    shouldShowSidebar () {
+    shouldShowSidebar() {
       const { frontmatter } = this.$page
       return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
+        !frontmatter.home &&
+        frontmatter.sidebar !== false &&
+        this.sidebarItems.length
       )
     },
 
-    sidebarItems () {
+    sidebarItems() {
       return resolveSidebarItems(
         this.$page,
         this.$page.regularPath,
@@ -118,40 +91,40 @@ export default {
       )
     },
 
-    pageClasses () {
+    pageClasses() {
       const userPageClass = this.$page.frontmatter.pageClass
       return [
         {
           'no-navbar': !this.shouldShowNavbar,
           'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
+          'no-sidebar': !this.shouldShowSidebar,
         },
-        userPageClass
+        userPageClass,
       ]
-    }
+    },
   },
 
-  mounted () {
+  mounted() {
     this.$router.afterEach(() => {
       this.isSidebarOpen = false
     })
   },
 
   methods: {
-    toggleSidebar (to) {
+    toggleSidebar(to) {
       this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
       this.$emit('toggle-sidebar', this.isSidebarOpen)
     },
 
     // side swipe
-    onTouchStart (e) {
+    onTouchStart(e) {
       this.touchStart = {
         x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
+        y: e.changedTouches[0].clientY,
       }
     },
 
-    onTouchEnd (e) {
+    onTouchEnd(e) {
       const dx = e.changedTouches[0].clientX - this.touchStart.x
       const dy = e.changedTouches[0].clientY - this.touchStart.y
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
@@ -161,7 +134,7 @@ export default {
           this.toggleSidebar(false)
         }
       }
-    }
-  }
+    },
+  },
 }
 </script>
